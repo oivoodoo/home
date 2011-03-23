9fd5a8e615d0e41f7da82beafebd12bd8ca7c191
Alexander Korsak <alex.korsak@gmail.com>
2011-03-23 14:05:51 +0000

    Add resource model

--- routes/admin.js
+++ routes/admin.js
@@ -6,6 +6,12 @@ module.exports = function(app) {
   app.all('/admin(/*)?', basicAuth(function(user, password){
     return user == 'admin' && password == 'admin';
   }));
+  
+  app.all('/admin(/*)?', function(req, res, next) {
+    Resource.find({}, function(err) {
+      if (err) return next(err);
+      });
+    });
 
   app.get('/admin(/)?', function(req, res, next){
     Post.count({}, function(err, count){
