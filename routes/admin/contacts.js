d359d4d2621bbb8a79dcd6c13e105c5611716120
Alexander Korsak <alex.korsak@gmail.com>
2011-03-29 11:58:11 +0000

    fix contacts admin page

--- routes/admin/contacts.js
+++ routes/admin/contacts.js
@@ -2,7 +2,6 @@ module.exports = function(app) {
   var Contact = app.Contact;
 
   app.get('/admin/contacts(/)?', function(req, res, next) {
-    console.log('Are you there?');
     Contact.find({}, function(err, contacts) {
       if (err) return next(err);
       res.render('admin/contacts/index', {
