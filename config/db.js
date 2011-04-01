969faedccc29bafe17ec9291469b18ca7179a3f7
Alexander Korsak <alex.korsak@gmail.com>
2011-04-02 00:19:30 +0300

    database connection updates

--- config/db.js
+++ config/db.js
@@ -2,16 +2,16 @@ var express = require('express');
 
 module.exports = function(app) {
   app.configure('development', function() {
-    app.set('db-uri', 'mongodb://admin:201287ali@flame.local.mongohq.com:27021/oivoodoo');
+    app.set('db-uri', 'mongodb://localhost/scores-development');
     app.use(express.errorHandler({ dumpExceptions: true }));
   });
   
   app.configure('test', function() {
-    app.set('db-uri', 'mongodb://admin:201287ali@flame.local.mongohq.com:27021/oivoodoo');
+    app.set('db-uri', 'mongodb://localhost/scores-test');
   });
   
   app.configure('production', function() {
-    app.set('db-uri', 'mongodb://admin:201287ali@flame.local.mongohq.com:27021/oivoodoo');
+    app.set('db-uri', 'mongodb://localhost/scores-production');
   });
   
   app.db = app.mongoose.connect(app.set('db-uri'));
