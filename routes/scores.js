7b268572ae303a9fea1c4cb6b26fb42e9e9bc9ab
Alexander Korsak <alex.korsak@gmail.com>
2012-05-03 21:54:04 +0300

    Change styles for the top scores page and increase scores count limit

--- routes/scores.js
+++ routes/scores.js
@@ -3,14 +3,16 @@ module.exports = function(app) {
 
   app.post('/create', function(req, res) {
     var score = new Score(req.body.score);
-    score.save(function(err) {
-      if (err) return next(err);
-    });
+    if (score.scores < 35000) { // magic number for saving scores
+      score.save(function(err) {
+        if (err) return next(err);
+      });
+    }
     res.send('OK+');
   });
 
   app.get('/top/json', function(req, res) {
-    var scores = Score.find().limit(13).sort('scores', -1).run(function(err, scores) {
+    var scores = Score.find().limit(30).sort('scores', -1).run(function(err, scores) {
       res.header("Access-Control-Allow-Origin", "*");
       if (!!scores) {
         res.send(scores.map(function(s){
