ff172c6a7538f933d674f0a32759d0783b6f28e4
Alexandr Korsak <alex.korsak@gmail.com>
2012-11-20 16:14:34 +0300

    Add render test

--- server.js
+++ server.js
@@ -23,6 +23,10 @@ var app = module.exports = express.createServer(
     // , connect.favicon()
 );
 
+app.get('/', function(req, res) {
+  res.render('text');
+});
+
 // app.configure(function() {
 //   app.set('views', __dirname + '/views');
 //   app.set('view engine', 'jade');
